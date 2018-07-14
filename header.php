2cd59d3404e222c69dd6190bef45d1a75fad512c
TobiHatti <32573581+TobiHatti@users.noreply.github.com>
2018-07-14 11:52:48 +0200

    Created CSS + Header beginning

--- header.php
+++ header.php
@@ -5,4 +5,27 @@
     require("data/functions.php");
 
 
+    echo '
+        <!DOCTYPE html>
+            <html>
+                <head>
+                    <link rel="stylesheet" type="text/css" href="css/style.css?2323">
+                    <link rel="stylesheet" type="text/css" href="css/designer.css?2323">
+                    <link rel="stylesheet" type="text/css" href="css/flags.css?2323">
+                    <link href="files/content/favicon.ico" rel="icon" type="image/x-icon" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                    <script src="/data/source.js?2323"></script>
+
+                       &copy;
+                       &Ouml;
+                       &szlig;
+
+                    <header>
+                    <main>
+                    <article>
+                    <nav>
+                    <footer>
+    ';
+
 ?>
