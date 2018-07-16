97008984678cf6ac8b31822e5c1768c0aa0d27f3
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-07-16 20:26:45 +0200

    Added Temporary page for organisation

---
+++ __pages.php
@@ -0,0 +1,29 @@
+<?php
+    require("header.php");
+
+        echo '
+        <main>
+            <h1>.php Pages</h1>
+            <br>
+            <code>
+                <table>
+                    <tr>
+                        <th>File</th>
+                        <th>Contents</th>
+                    </tr>
+                    <tr>
+                        <td>index.php</td>
+                        <td>Startseite</td>
+                    </tr>
+                    <tr>
+                        <td>index.php</td>
+                        <td>Startseite</td>
+                    </tr>
+                </table>
+            </code>
+        </main>
+
+        ';
+
+    include("footer.php")
+?>
