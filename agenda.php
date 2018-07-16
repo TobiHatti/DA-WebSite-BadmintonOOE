15c6887791bcd05d9a20c2b39e85050dbbc15a06
paull <paull@DESKTOP-DVL4DTN>
2018-07-16 21:56:58 +0200

    Agenda successfully integrated with google calenders

--- agenda.php
+++ agenda.php
@@ -9,7 +9,7 @@
 
 <head>
   <title>Terminkalender | O&Ouml;. Badmintonverband</title>
-
+   <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=de.christian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=de.austrian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=e_2_de%23weeknum%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Europe%2FVienna" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
 </head>
 
@@ -17,7 +17,7 @@
 
 <?php
 
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=de.christian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=de.austrian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=e_2_de%23weeknum%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Europe%2FVienna" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
 echo ('</main>');
 include ("footer.php");
 
