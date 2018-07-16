e1657cce695d635e158353e90631379bac1369de
paull <paull@DESKTOP-DVL4DTN>
2018-07-16 20:09:07 +0200

    started a new agenda, because the other one sucked

--- agenda.php
+++ agenda.php
@@ -1,225 +1,110 @@
+<?php
+
+/*
+   Simple Calendar
+   by Karlis Blumentals (www.blumentals.net)
+
+   Displays a calendar, one month at a time.
+   Selected date is passed as parameter day=yyyy-mm-dd
+   e.g. myscript.php?day=2012-11-22
+*/
+
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title>Calendar</title>
+  <style>
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  a {
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }  
+  </style>
+</head>
+
+<body>
 
 <?php
-    require("header.php");
 
-    echo'
-
-        class Calendar {
-
-    /**
-     * Constructor
-     */
-    public function __construct(){
-        $this->naviHref = htmlentities($_SERVER['PHP_SELF']);
-    }
-
-    /********************* PROPERTY ********************/
-    private $dayLabels = array("Mon","Tue","Wed","Thu","Fri","Sat","Sun");
-
-    private $currentYear=0;
-
-    private $currentMonth=0;
-
-    private $currentDay=0;
-
-    private $currentDate=null;
-
-    private $daysInMonth=0;
-
-    private $naviHref= null;
-
-    /********************* PUBLIC **********************/
-
-    /**
-    * print out the calendar
-    */
-    public function show() {
-        $year  == null;
-
-        $month == null;
-
-        if(null==$year&&isset($_GET['year'])){
-
-            $year = $_GET['year'];
-
-        }else if(null==$year){
-
-            $year = date("Y",time());
-
-        }
-
-        if(null==$month&&isset($_GET['month'])){
-
-            $month = $_GET['month'];
-
-        }else if(null==$month){
-
-            $month = date("m",time());
-
-        }
-
-        $this->currentYear=$year;
-
-        $this->currentMonth=$month;
-
-        $this->daysInMonth=$this->_daysInMonth($month,$year);
-
-        $content='<div id="calendar">'.
-                        '<div class="box">'.
-                        $this->_createNavi().
-                        '</div>'.
-                        '<div class="box-content">'.
-                                '<ul class="label">'.$this->_createLabels().'</ul>';
-                                $content.='<div class="clear"></div>';
-                                $content.='<ul class="dates">';
-
-                                $weeksInMonth = $this->_weeksInMonth($month,$year);
-                                // Create weeks in a month
-                                for( $i=0; $i<$weeksInMonth; $i++ ){
-
-                                    //Create days in a week
-                                    for($j=1;$j<=7;$j++){
-                                        $content.=$this->_showDay($i*7+$j);
-                                    }
-                                }
-
-                                $content.='</ul>';
-
-                                $content.='<div class="clear"></div>';
-
-                        $content.='</div>';
-
-        $content.='</div>';
-        return $content;
-    }
-
-    /********************* PRIVATE **********************/
-    /**
-    * create the li element for ul
-    */
-    private function _showDay($cellNumber){
-
-        if($this->currentDay==0){
-
-            $firstDayOfTheWeek = date('N',strtotime($this->currentYear.'-'.$this->currentMonth.'-01'));
-
-            if(intval($cellNumber) == intval($firstDayOfTheWeek)){
-
-                $this->currentDay=1;
-
-            }
-        }
-
-        if( ($this->currentDay!=0)&&($this->currentDay<=$this->daysInMonth) ){
-
-            $this->currentDate = date('Y-m-d',strtotime($this->currentYear.'-'.$this->currentMonth.'-'.($this->currentDay)));
-
-            $cellContent = $this->currentDay;
-
-            $this->currentDay++;
-
-        }else{
-
-            $this->currentDate =null;
-
-            $cellContent=null;
-        }
-
-
-        return '<li id="li-'.$this->currentDate.'" class="'.($cellNumber%7==1?' start ':($cellNumber%7==0?' end ':' ')).
-                ($cellContent==null?'mask':'').'">'.$cellContent.'</li>';
-    }
-
-    /**
-    * create navigation
-    */
-    private function _createNavi(){
-
-        $nextMonth = $this->currentMonth==12?1:intval($this->currentMonth)+1;
-
-        $nextYear = $this->currentMonth==12?intval($this->currentYear)+1:$this->currentYear;
-
-        $preMonth = $this->currentMonth==1?12:intval($this->currentMonth)-1;
-
-        $preYear = $this->currentMonth==1?intval($this->currentYear)-1:$this->currentYear;
-
-        return
-            '<div class="header">'.
-                '<a class="prev" href="'.$this->naviHref.'?month='.sprintf('%02d',$preMonth).'&year='.$preYear.'">Prev</a>'.
-                    '<span class="title">'.date('Y M',strtotime($this->currentYear.'-'.$this->currentMonth.'-1')).'</span>'.
-                '<a class="next" href="'.$this->naviHref.'?month='.sprintf("%02d", $nextMonth).'&year='.$nextYear.'">Next</a>'.
-            '</div>';
-    }
-
-    /**
-    * create calendar week labels
-    */
-    private function _createLabels(){
-
-        $content='';
-
-        foreach($this->dayLabels as $index=>$label){
-
-            $content.='<li class="'.($label==6?'end title':'start title').' title">'.$label.'</li>';
-
-        }
-
-        return $content;
-    }
-
-
-
-    /**
-    * calculate number of weeks in a particular month
-    */
-    private function _weeksInMonth($month=null,$year=null){
-
-        if( null==($year) ) {
-            $year =  date("Y",time());
-        }
-
-        if(null==($month)) {
-            $month = date("m",time());
-        }
-
-        // find number of days in this month
-        $daysInMonths = $this->_daysInMonth($month,$year);
-
-        $numOfweeks = ($daysInMonths%7==0?0:1) + intval($daysInMonths/7);
-
-        $monthEndingDay= date('N',strtotime($year.'-'.$month.'-'.$daysInMonths));
-
-        $monthStartDay = date('N',strtotime($year.'-'.$month.'-01'));
-
-        if($monthEndingDay<$monthStartDay){
-
-            $numOfweeks++;
-
-        }
-
-        return $numOfweeks;
-    }
-
-    /**
-    * calculate number of days in a particular month
-    */
-    private function _daysInMonth($month=null,$year=null){
-
-        if(null==($year))
-            $year =  date("Y",time());
-
-        if(null==($month))
-            $month = date("m",time());
-
-        return date('t',strtotime($year.'-'.$month.'-01'));
-    }
-
+error_reporting(E_ALL ^ E_NOTICE);
+date_default_timezone_set('UTC');
+
+// date ok flag
+$dateok = false;
+
+// parse parameter
+if (isset($_GET['day'])) {
+  list($yr, $mo, $da) = explode('-', $_GET['day']);
+  $yr = intval($yr);
+  $mo = intval($mo);
+  $da = intval($da);
+  if (checkdate($mo, $da, $yr)) $dateok = true;
 }
 
+// if invalid date selected then selected date = today
+if (!$dateok) {
+  $mo = date('m');
+  $da = date('d');
+  $yr = date('Y');
+}
 
+$offset = date('w', mktime(0,0,0,$mo,1,$yr));
+// we must have a value in range 1..7
+if ($offset == 0) $offset = 7;
 
+// days in month
+$nd = date('d', mktime(0,0,0,$mo+1,0,$yr));
 
+// days array
+$days = array();
 
+// reset array
+for ($i=0;$i<=42;$i++) $days[$i]['out']= '&nbsp;';
 
-    '
+// fill days array
+// valid days contain data, invalid days are left blank
+$j=1;
+for ($i=$offset;$i<=($offset+$nd-1);$i++) {
+  $day = $j++;
+  $date = $yr.'-'.$mo.'-'.$day;
+  $days[$i]['out']= '<a href="?day='.$date.'">'.$day.'</a>';
+  $days[$i]['dat']= $date;
+}
+
+// output table
+echo('<table style="text-align: center; border-collapse: collapse; width: 200px">');
+echo('<tr>');
+echo('<td colspan="1" style="border: 1px solid Silver;"><a href="?day='.date('Y-m-d', mktime(0,0,0,$mo,$da,$yr-1)).'">&laquo;</a></td>');
+echo('<td colspan="5" style="border: 1px solid Silver;"><p class="calendar">'.$yr.'</p></td>');
+echo('<td colspan="1" style="border: 1px solid Silver;"><a href="?day='.date('Y-m-d', mktime(0,0,0,$mo,$da,$yr+1)).'">&raquo;</a></td>');
+echo('</tr>'."\n");
+echo('<tr>');
+echo('<td colspan="1" style="border: 1px solid Silver;"><a href="?day='.date('Y-m-d', mktime(0,0,0,$mo,0,$yr)).'">&laquo;</a></td>');
+echo('<td colspan="5" style="border: 1px solid Silver;"><p style="font-weight: bold">'.date('F', mktime(0,0,0,$mo,$da,$yr)).'</p></td>');
+echo('<td colspan="1" style="border: 1px solid Silver;"><a href="?day='.date('Y-m-d', mktime(0,0,0,$mo+1,1,$yr)).'">&raquo;</a></td>');
+echo('</tr>'."\n");
+$cntr = 1; // day printing counter
+for ($i=1;$i<=6;$i++) {
+  echo('<tr>');
+  for ($j=1;$j<=7;$j++) {
+    $curr = $cntr++;
+    if ($days[$curr]['dat'] == $yr.'-'.$mo.'-'.$da) $style = 'bold'; else $style = 'normal';
+    echo('<td style="border: 1px solid Silver; width: 14%; font-weight: '.$style.'">'.$days[$curr]['out'].'</td>'."\n");
+  }
+  echo('</tr>'."\n");
+}
+echo('</table>');
 ?>
+
+</body>
+
+</html>
