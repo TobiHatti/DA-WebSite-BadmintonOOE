63a415b09dfb2068fe7e0f4ce29cfa7b502fe98b
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-07-17 18:32:12 +0200

    Restructuring of agenda.php

--- agenda.php
+++ agenda.php
@@ -9,15 +9,14 @@
 
 <head>
   <title>Terminkalender | O&Ouml;. Badmintonverband</title>
-<<<<<<< HEAD
-=======
-   <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=de.christian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=de.austrian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=e_2_de%23weeknum%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Europe%2FVienna" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
->>>>>>> 15c6887791bcd05d9a20c2b39e85050dbbc15a06
+
+
 </head>
 
-
-
+    <center>
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=de.christian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=de.austrian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=e_2_de%23weeknum%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Europe%2FVienna" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </center>
 <?php
 
 
