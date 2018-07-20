f17b46d10f0291d7a5bb84e2027d720803a518a5
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-07-20 22:14:47 +0200

    Started Designing for all Form-Elements

--- __tempform.php
+++ __tempform.php
@@ -2,18 +2,40 @@
     require("header.php");
 
     echo '
-        <div class="stagfade1"><h1>Temporary Form-File</h1></div>
+        <h1 class="stagfade1">Temporary Form-File</h1>
 
-        <h3>Use temporarily for SQL/PHP Forms and Database insertions</h3>
-
+        <h3 class="stagfade2">Use temporarily for SQL/PHP Forms and Database insertions</h3>
+        <br><br><br>
 
         <form action="'.ThisPage().'" method="post" accept-charset="utf-8" enctype="multipart/form-data">
 
+            button<input type="button" value="hallo i bims ein knopf"/><br><br>
+            checkbox<input type="checkbox"/><br><br>
+            color<input type="color"/><br><br>
+            date<input type="date"/><br><br>
+            datetime-local<input type="datetime-local"/><br><br>
+            email<input type="email"/><br><br>
+            file<input type="file"/><br><br>
+            month<input type="month"/><br><br>
+            number<input type="number"/><br><br>
+            password<input type="password"/><br><br>
+            radio<input type="radio"/><br><br>
+            range<input type="range"/><br><br>
+            reset<input type="reset"/><br><br>
+            search<input type="search"/><br><br>
+            submit<input type="submit"/><br><br>
+            tel<input type="tel"/><br><br>
+            text<input type="text"/><br><br>
+            time<input type="time"/><br><br>
+            url<input type="url"/><br><br>
+            week<input type="week"/><br><br>
 
 
+            <br><br><br><br>
 
+            <button type="submit">BESTÄTIGEN</button>
 
-
+        </form>
 
 
 
