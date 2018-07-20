1b5ff5418a7d692d16fba3e9e589e5809b09c42b
paull <paull@DESKTOP-DVL4DTN>
2018-07-20 19:11:28 +0200

    Vorstand fully added the people (needs to be right designed)

--- vorstand.php
+++ vorstand.php
@@ -243,11 +243,154 @@
                 <span style="color: #444444">0699/11138311</span>
 
             </div   >
+
+             <div>
+                 <img src="/content/Vorstand/harald_starl.jpg" alt=" Harald Starl" class="user_img_m"/>
+             </div>
+             <div>
+                <span style="color: #AD0000"><b> Harald Starl </b></span>
+                <br>
+                <i><span style="color: #444444">Rechnungspr&uuml;fer</span></i>
+                <br>
+                <a href="mailto:starl@badminton.at">E-Mail senden</a>
+                <br>
+                <span style="color: #444444">Mobiltelefon:</span>
+                <br>
+                <span style="color: #444444">0676/5117052</span>
+
+            </div   >
+
+            <div>
+                 <img src="/content/Vorstand/josef_moser.jpg" alt=" Josef Moser" class="user_img_m"/>
+             </div>
+             <div>
+                <span style="color: #AD0000"><b> Kons. Josef Moser </b></span>
+                <br>
+                <i><span style="color: #444444">Rechnungspr&uuml;fer</span></i>
+                <br>
+                <a href="mailto:pepi-moser@gmx.at">E-Mail senden</a>
+                <br>
+                <span style="color: #444444">Mobiltelefon:</span>
+                <br>
+                <span style="color: #444444">0676/81424577</span>
+
+            </div   >
+
+            <div>
+                 <img src="/content/Vorstand/heinz_lahnsteiner.jpg" alt=" Karl Heinz Lahnsteiner" class="user_img_m"/>
+             </div>
+             <div>
+                <span style="color: #AD0000"><b> Dr. Karl Heinz Lahnsteiner </b></span>
+                <br>
+                <i><span style="color: #444444">Rechtsausschuss</span></i>
+                <br>
+                <a href="mailto:elahn@tele2.at">E-Mail senden</a>
+                <br>
+                <span style="color: #444444">Telefon:</span>
+                <br>
+                <span style="color: #444444">06133/5354</span>
+
+            </div   >
+
+            <div>
+                 <img src="/content/Vorstand/karl_proell.jpg" alt=" Karl Pröll" class="user_img_m"/>
+             </div>
+             <div>
+                <span style="color: #AD0000"><b> Karl Pr&ouml;ll </b></span>
+                <br>
+                <i><span style="color: #444444">Rechtsausschuss</span></i>
+                <br>
+                <i><span style="color: #444444">Rechnungspr&uuml;fer</span></i>
+                <br>
+                <a href="mailto:starl@badminton.at">E-Mail senden</a>
+                <br>
+                <span style="color: #444444">Mobiltelefon:</span>
+                <br>
+                <span style="color: #444444">0676/814276220</span>
+
+            </div >
             <br>
             <br>
             <br>
 
+            <div>
+               <span style="color: #AD0000"><b> Nachwuchsarbeiterkreis </b></span>
+               <br>
+               <span style="color: #000000">Otmar Auberger</span>
+               <br>
+               <span style="color: #000000">Stephan Dlapka</span>
+               <br>
+               <span style="color: #000000">Alexander Mittermayr</span>
+               <br>
+               <span style="color: #000000">Klaus Niederhuber</span>
+               <br>
+               <span style="color: #000000">Peter Schwarzmann</span>
+               <br>
+               <span style="color: #000000">Sepp Retschitzegger</span>
+               <br>
+               <span style="color: #000000">Hermann Schausberger</span>
+               <br>
+               <span style="color: #000000">Peter Kickinger</span>
+               <br>
+               <span style="color: #000000">Dieter Stockinger</span>
+               <br>
+               <span style="color: #000000">Phillip Stelzm&uuml;ller</span>
+               <br>
+               <span style="color: #000000">Fritz Freim&uuml;ller</span>
+               <br>
+               <span style="color: #000000">Daniel Walchshofer</span>
+            </div>
 
+            <div>
+               <br>
+               <span style="color: #AD0000"><b>Ligagremium</b></span>
+               <br>
+               <span style="color: #000000">Klaus Mitterhauser</span>
+               <br>
+               <span style="color: #000000">Hermann Schausberger</span>
+               <br>
+               <span style="color: #000000">Peter Krenmayr</span>
+            </div>
+
+            <div>
+               <br>
+               <span style="color: #AD0000"><b>Ligaausschuss</b></span>
+               <br>
+               <span style="color: #000000">Andreas Rabel</span>
+               <br>
+               <span style="color: #000000">Harald Starl</span>
+               <br>
+               <span style="color: #000000">Karl Pr&ouml;ll</span>
+               <br>
+               <span style="color: #000000">Hermann Schausberger</span>
+               <br>
+               <span style="color: #000000">Markus Eckersberger</span>
+               <br>
+               <span style="color: #000000">Michael Kaun</span>
+               <br>
+               <span style="color: #000000">Klaus Mitterhauser</span>
+            </div>
+
+            <div>
+                <br>
+               <span style="color: #AD0000"><b>Schiedsgericht</b></span>
+               <br>
+               <span style="color: #000000">Sandra Kranzl</span>
+               <br>
+               <span style="color: #000000">Wolfgang Hofreiter</span>
+               <br>
+               <span style="color: #000000">Manfred Berger-S&ouml;llinger</span>
+               <br>
+               <span style="color: #000000">Christian Klement</span>
+               <br>
+               <span style="color: #000000">Franz G&ouml;sweiner</span>
+               <br>
+               <span style="color: #000000">Martin Grafinger</span>
+               <br>
+               <span style="color: #000000">Richard Z&ouml;llner</span>
+               <br>
+               <span style="color: #000000">Stefan Holl</span>
+            </div>
          <aside></aside>
     ';
 
