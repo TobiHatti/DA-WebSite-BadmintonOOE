12750d349598fcb963cb04884db903d31621843e
paull <paull@DESKTOP-DVL4DTN>
2018-07-20 20:24:22 +0200

    Added links to the page

--- links.php
+++ links.php
@@ -2,7 +2,81 @@
     require("header.php");
     PageTitle("Links");
 
-    echo '<h1 class="stagfade1">Links</h1>';
+    echo '<h1 class="stagfade1">Links</h1>
+
+     <div>
+        <span style="color: #555555"><b>Vereine:</b></span>
+        <br>
+        <a href="http://www.askoe-linz-badminton.at/">ASK&Ouml; Linz Badminton</a>
+        <br>
+        <a href="https://www.badminton-alkoven.at/">BC Schweiger-Sport Alkhoven</a>
+        <br>
+        <a href="https://www.badminton-skvoest.at/">Sk V&ouml;st</a>
+        <br>
+        <a href="http://www.badminton-wels.at/">ABV Wels</a>
+        <br>
+        <a href="http://www.bsc70linz.at/cms/">BSC 70 Linz</a>
+        <br>
+        <a href="https://unionurfahrbadminton.wordpress.com/">Union Urfahr</a>
+        <br>
+        <a href="https://www.badminton-alkoven.at/">BC M&uuml;nzkirchen</a>
+        <br>
+        <a href="https://ask-nettingsdorf-badminton.at/">ASK Netingsdorf</a>
+        <br>
+        <a href="https://www.badminton-windischgarsten.at/web/">Union Windischgarsten</a>
+        <br>
+        <a href="http://www.bc-windischgarsten.at/">BC Windischgarsten</a>
+        <br>
+        <a href="http://kirchdorf.sportunion.at/start.php?contentID=7893&v1=force">Sportunion Kirchdorf</a>
+        <br>
+        <a href="http://www.sportunion-ohlsdorf.at/">Sportunion Ohlsdorf</a>
+        <br>
+        <a href="http://ubc-neuhofen.at/">UBC Neuhofen</a>
+     </div>
+
+     <div>
+        <span style="color: #555555"><b>Verb&auml;nde:</b></span>
+        <br>
+        <a href="http://www.badminton.at/">&Ouml;sterreichischer Badmintonverband</a>
+        <br>
+        <a href="http://obv.tournamentsoftware.com/Home">&Ouml;BV Ergebnisdienst</a>
+        <br>
+        <a href="http://www.badminton-noe.at/">Nieder&ouml;sterreichischer Badmintonverband</a>
+        <br>
+        <a href="http://www.sbv.at/">Salzbuger Badmintonverband</a>
+        <br>
+        <a href="http://www.badminton-tirol.at/">Tiroler Badmintonverband</a>
+        <br>
+        <a href="https://www.badminton-vorarlberg.at/clubdesk/www">Vorarlberger Badmintonverband</a>
+        <br>
+        <a href="https://www.wienbadminton.at/">Wiener Badmintonverband</a>
+        <br>
+        <a href="http://badminton-steiermark.at/">Steirischer Badmintonverband</a>
+     </div>
+
+     <div>
+        <span style="color: #555555"><b>International:</b></span>
+        <br>
+        <a href="http://www.internationalbadminton.org/">Badminton World Federation</a>
+        <br>
+        <a href="http://www.eurobadminton.org/">EBU-European Badminton Union</a>
+        <br>
+        <a href="http://www.badzine.de/">BadZine.de</a>
+        <br>
+        <a href="http://www.badmintoneurope.com/cms/?&pageid=5960">Magazine - Badminton Europe</a>
+     </div>
+
+     <div>
+        <span style="color: #555555"><b>Partner:</b></span>
+        <br>
+        <a href="http://www.sportland-ooe.at/">O&Ouml;. Landessportorganisation</a>
+        <br>
+        <a href="http://www.sportkoch.at/index.php">Sportkoch</a>
+     </div>
+
+    ';
+
+
 
     include("footer.php");
 ?>
