da561ea82bf26e499701b321603bf4bc6a6c82ab
paull <paull@DESKTOP-DVL4DTN>
2018-07-21 23:02:04 +0200

    added links to schueler-jugend.php

--- schueler-jugend.php
+++ schueler-jugend.php
@@ -2,7 +2,75 @@
     require("header.php");
     PageTitle("Sch&uuml;ler / Jugend");
 
-    echo '<h1 class="stagfade1">Sch&uuml;ler / Jugend</h1>';
+    echo '<h1 class="stagfade1">Sch&uuml;ler / Jugend</h1>
+
+    <p>
+        <br>
+        <h5><span style="color: #CC0000">O&Ouml;MM-Sch&uuml;ler - Termin: 08.12.2017 - Vorchdorf</span></h5><br>
+        <a href="http://obv.tournamentsoftware.com/sport/tournament.aspx?id=5D9634C9-9D9F-4778-8D04-5BC2E6608DC0">Spieltermine in Tournamentsoftware</a><br>
+    </p>
+    <br>
+    <hr>
+    <br>
+
+    <p>
+        <h5><span style="color: #CC0000">O&Ouml;MM-Jugend - Termin: 8.12.2016 - Traun</span></h5>
+        <h5><span style="color: #CC0000">O&Ouml;MM-Sch&uuml;ler - Termin: 17.12.2016 - Vorchdorf</span></h5><br>
+        <a href="http://obv.tournamentsoftware.com/sport/tournament.aspx?id=C5983872-69F8-4E1B-A463-CB1DB1323A39">Spieltermine in Tournamentsoftware</a>
+    </p>
+    <br>
+    <hr>
+    <br>
+
+    <p>
+       <h5><span style="color: #CC0000">O&Ouml;MM-Sch&uuml;ler - Termin: 12.12.2015 - Vorchdorf</span></h5><br>
+       <a href="http://obv.tournamentsoftware.com/sport/tournament.aspx?id=7EC143B8-C64E-477B-9DDC-3772C6AED715">Spieltermine in Tournamentsoftware</a><br><br>
+       <h5><span style="color: #CC0000">O&Ouml;MM-Jugend - Termin: 5.6.2016 - Ohlsdorf</span></h5><br>
+       <a href="http://obv.tournamentsoftware.com/sport/tournament.aspx?id=10E32288-7368-42AC-98C8-94E6BC7D1528">Spieltermine in Tournamentsoftware</a><br>
+    </p>
+    <br>
+    <hr>
+    <br>
+
+    <p>
+      <h5><span style="color: #CC0000">O&Ouml;MM-Jugend - Termin: 13.12.2014 - Vorchdorf</span></h5><br>
+      <a href="http://obv.tournamentsoftware.com/sport/event.aspx?id=DBAB0794-0899-47C0-80C5-2A5C094224EF&event=1">Spieltermine in Tournamentsoftware</a><br><br>
+      <h5><span style="color: #CC0000">O&Ouml;MM-Sch&uuml;ler - Termin: 2.3.2014 - Ohlsdorf</span></h5><br>
+      <a href="http://obv.tournamentsoftware.com/sport/event.aspx?id=442CEBD4-1D76-430A-95BD-D805DD8CD187&event=2">Spieltermine in Tournamentsoftware</a><br>
+    </p>
+    <br>
+    <hr>
+    <br>
+
+    <p>
+      <h5><span style="color: #CC0000">O&Ouml;MM-Jugend - Termin: 14.12.2013 - Vorchdorf</span></h5><br>
+      <h5><span style="color: #CC0000">O&Ouml;MM-Sch&uuml;ler - Termin: 2.3.2014 - Traun Bundesschulzentrum</span></h5><br>
+      <a href="http://obv.tournamentsoftware.com/sport/event.aspx?id=2646EC70-790A-4288-BE8D-7977E14C6695&event=1">Spieltermine in Tournamentsoftware</a><br>
+    </p>
+    <br>
+    <hr>
+    <br>
+
+    <p>
+      <h5><span style="color: #CC0000">Termin: 16.3.2013 - Traun Bundesschulzentrum </span></h5><br>
+      <h5><span style="color: #CC0000">Austragung der Sch&uuml;ler und Jugendmannschaftsmeisterschaft 2013 in Turnierform</span></h5><br>
+      <a href="http://www.badminton-ooe.at/wp-content/uploads/SCH-JD-MM-2013-Spielerliste1.pdf">Spielerliste Sch&uuml;ler-Jugend MM 2013 (aktuelle Version)</a><br>
+      <a href="http://obv.tournamentsoftware.com/sport/event.aspx?id=6077C0DD-2D11-49A2-B0A0-B11B4A3B9D98&event=1">Spieltermine in Tournamentsoftware</a><br>
+    </p>
+    <br>
+    <hr>
+    <br>
+
+    <p>
+       <a href="http://www.badminton-ooe.at/wp-content/uploads/2011/11/Auslosung-Nachwuchs-MM-11-12-Sch%C3%BClerJugend.pdf">Auslosung 2011-2012</a><br>
+       <a href="http://www.badminton-ooe.at/wp-content/uploads/2012/01/SCH-JD-MM-2012Spielerrangliste.pdf">Spielerliste Sch&uuml;ler-Jugend MM 2012</a><br>
+       <a href="http://obv.tournamentsoftware.com/sport/tournament.aspx?id=C0FEF8A0-02BC-4092-A794-F0E5902EBF53">Spieltermine in Tournamentsoftware</a><br>
+    </p>
+
+
+
+
+    ';
 
     include("footer.php");
 ?>
