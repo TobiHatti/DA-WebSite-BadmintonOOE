3cee196bf3d568879438853cbe75e8ce13246318
paull <paull@DESKTOP-DVL4DTN>
2018-08-05 23:45:20 +0200

    added download links (needs to be finished by design), added vereine to the subproject (design needs to be made)

--- downloads.php
+++ downloads.php
@@ -2,7 +2,19 @@
     require("header.php");
     PageTitle("Downloads");
 
-    echo '<h1 class="stagfade1">Downloads</h1>';
+    echo '<h1 class="stagfade1">Downloads</h1>
+
+    <p>
+       <a target="_blank" href="C:\xampp\htdocs\Badminton\content\Excel-Data\downloads" onmouseover="ChangeFrameLink(\'http://www.askoe-linz-badminton.at/\');">Letztverbraucherliste NEU <i>(EXCEL Verlinkungen???)</i></a>
+       <br>
+       <br>
+       <a target="_blank" href="C:\xampp\htdocs\Badminton\content\Excel-Data\downloads" onmouseover="ChangeFrameLink(\'http://www.askoe-linz-badminton.at/\');">Tournament Raster Tool<i>(EXCEL Verlinkungen???)</i></a>
+    </p>
+
+
+
+
+    ';
 
     include("footer.php");
 ?>
--- vereine.php
+++ vereine.php
@@ -2,7 +2,344 @@
     require("header.php");
     PageTitle("Vereine");
 
-    echo '<h1 class="stagfade1">Vereine</h1>';
+    echo '<h1 class="stagfade1">Vereine</h1>
+
+    <div>
+       Alphabetisch nach Ort sortiert:
+       <a href="#alkhoven">A</a>
+       |
+       <a href="#braunau">B</a>
+       |
+       <a href="#freistadt">F</a>
+       |
+       <a href="#kematen">K</a>
+       |
+       <a href="#linkzaskoe">L</a>
+       |
+       <a href="#marienkirchen">M</a>
+       |
+       <a href="#nettingsdorf">N</a>
+       |
+       <a href="#ohlsdorf">O</a>
+       |
+       <a href="#pasching">P</a>
+       |
+       <a href="#ried">R</a>
+       |
+       <a href="#schlierbach">S</a>
+       |
+       <a href="#traun">T</a>
+       |
+       <a href="#urfahr">U</a>
+       |
+       <a href="#vorchdorf">V</a>
+       |
+       <a href="#wels">W</a>
+    </div>
+
+    <hr>
+    <div>
+         <h4 style="margin-top: 0px;">
+            <a href="#alkhoven" name="alkhoven">BC Raiffeisen Alkhoven</a>
+         </h4>
+         <br>
+         Kennzahl: 326
+         <br>
+         Dachverband: ASK&Ouml;
+         <br>
+         <br>
+         <b>Fritz Freim&uuml;ller</b>
+         <br>
+         Veilchenstra&szlig;e 19
+         <br>
+         4072 Alkhoven
+         <br>
+         Internet: <a href="https://www.badminton-alkoven.at/">www.badminton-alkhoven.at</a>
+         <br>
+         E-mail: <a href="mailto:f.freimueller@eduhi.at">f.freimueller@eduhi.at</a>
+         <br>
+         Mobil: 0664 5340799
+    </div>
+
+    <br>
+    <br>
+    <br>
+
+    <div>
+        <h4 style="margin-top: 0px;">
+            <a href="#altmuenster" name="altmuenster">Sportunion Altm&uuml;nster</a>
+        </h4>
+        <br>
+        Kennzahl: 344
+        <br>
+        Dachverband: Sportunion
+        <br>
+        <br>
+        <b>Peter Kickinger</b>
+        <br>
+
+        Eben 72
+        <br>
+        4813 Altm&uuml;nster
+        <br>
+        E-mail: <a href="mailto:peter.kickinger@aon.at">peter.kickinger@aon.at</a>
+        <br>
+        Mobil: 0660 6760427
+
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+        <h4 style="margin-top: 0px;">
+            <a href="#andorf" name="andorf">&Ouml;TB - Allgemeiner Turnverein Andorf</a>
+        </h4>
+        <br>
+        Kennzahl: 301
+        <br>
+        Dachverband: ASV&Ouml;
+        <br>
+        <br>
+        <b>Christina Hofinger</b>
+        <br>
+        Max-Langgruber-Stra&szlig;e 15/3
+        <br>
+        4770 Andorf
+        <br>
+        Internet: <a href="http://www.badminton-andorf.at/">www.badminton-andorf.at</a>
+        <br>
+        E-mail: <a href="mailto:christina.hofinger@gmx.at">christina.hofinger@gmx.at</a>
+        <br>
+        Mobil: 0676 7766884
+        <br>
+        Telefon Privat: 07766 2762
+
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+        <h4 style="margin-top: 0px;">
+            <a href="#attergau" name="attergau">USC Attergau</a>
+        </h4>
+        <br>
+        Kennzahl: 322
+        <br>
+        Dachverband: Sportunion
+        <br>
+        <br>
+        <b>Klaus Baumann</b>
+        <br>
+        Attergauerstra&szlig;e 99
+        <br>
+        4880 St. Georgen/A.
+        <br>
+        E-mail: <a href="mailto:klaus.baumann1968@gmail.com">klaus.baumann1968@gmail.com</a>
+        <br>
+        Mobil: 0676 5009399
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+        <h4 style="margin-top: 0px;">
+            <a href="#braunau" name="braunau">Union VKB Braunau</a>
+        </h4>
+        <br>
+        Kennzahl: 310
+        <br>
+        Dachverband: Sportunion
+        <br>
+        <br>
+        <b>Stephan Kundl</b>
+        <br>
+        Mitterweg 11
+        <br>
+        5282 Ranshofen
+        <br>
+        E-mail: <a href="mailto:badminton.braunau@googlemail.com">badminton.braunau@googlemail.com</a>
+        <br>
+        Mobil: 0650 4977137
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+        <h4 style="margin-top: 0px;">
+            <a href="#kematen" name="kematen">SV Kemanten am Innbach</a>
+        </h4>
+        <br>
+        Kennzahl: 313
+        <br>
+        Dachverband: ASV&Ouml;
+        <br>
+        <br>
+        <b>Manfred Berger-S&ouml;llinger</b>
+        <br>
+        Gr&uuml;bl 14
+        <br>
+        4633 Kemnaten am Innbach
+        <br>
+        E-mail: <a href="mailto:bergersoellingermanfred@gmx.at">bergersoellingermanfred@gmx.at</a>
+        <br>
+        Mobil: 0664 2523750
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+         <h4 style="margin-top: 0px;">
+            <a href="#kirchdorf" name="kirchdorf">&Ouml;sterreichische Turn- und Sportunion Kirchdorf</a>
+        </h4>
+        <br>
+        Kenzzahl: 330
+        <br>
+        Dachverband: Sportunion
+        <br>
+        <br>
+        <b>Sandra Kranzl</b>
+        <br>
+        4560 Kirchdorf/Krems
+        <br>
+        Internet: <a href="http://www.kirchdorf.sportunion.at/">www.kirchdorf.sportunion.at</a>
+        <br>
+        E-mail: <a href="mailto: sandra.kranzl@gmail.com">sandra.kranzl@gmail.com</a>
+        <br>
+        Mobil: 0676 6095018
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+        <h4 style="margin-top: 0px;">
+            <a href="#linzaskoe" name="linzaskoe">ASK&Ouml; Linz-Badminton</a>
+        </h4>
+        <br>
+        Kennzahl: 303
+        <br>
+        Dachverband: ASK&Ouml;
+        <br>
+        <br>
+        <b>Manfred Peterseil</b>
+        <br>
+        Stockholmweg 36/1/5
+        <br>
+        4040 Linz
+        <br>
+        Internet: <a href="http://www.bsc70linz.at/cms/">www.bsc70linz.at</a>
+        <br>
+        E-mail: <a href="mailto: office@bsc70linz.at">office@bsc70linz.at</a>
+        <br>
+        Mobil: 0664 8411881
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+       <h4 style="margin-top: 0px;">
+            <a href="#linzbsc70" name="linzbsc70">BSC 70 Linz</a>
+        </h4>
+        <br>
+        Kennzahl: 304
+        <br>
+        Dachverband: ASK&Ouml;
+        <br>
+        <br>
+        <b>Reinhard Hechenberger</b>
+        <br>
+        Brennerstra&szlig;e 29
+        <br>
+        4040 Linz
+        <br>
+        Internet: <a href="http://www.bsc70linz.at/cms/">www.bsc70linz.at</a>
+        <br>
+        E-mail: <a href="mailto:office@bsc70linz.at">office@bsc70linz.at</a>
+
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+       <h4 style="margin-top: 0px;">
+            <a href="#linzvoeest" name="linzvoeest">SK V&ouml;st Linz</a>
+        </h4>
+        <br>
+        Kennzahl: 338
+        <br>
+        Dachverband: ASK&Ouml;
+        <br>
+        <br>
+        <b>Herbert Basting</b>
+        <br>
+        Wambacherstra&szlig;e 218
+        <br>
+        4030 Linz
+        <br>
+        Internet: <a href="https://www.badminton-skvoest.at/">www.badminton-skvoest.at</a>
+        <br>
+        E-mail: <a href="mailto: herbert.basting@aon.at">herbert.basting@aon.at</a>
+        <br>
+        Mobil: 0680 2165490
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+        <h4 style="margin-top: 0px;">
+            <a href="#marienkirchen" name="marienkirchen">UNION St.Marienkirchen am Hausruck</a>
+        </h4>
+        <br>
+        Kennzahl: 334
+        <br>
+        Dachverband: Sportunion
+        <br>
+        <br>
+        <b>Christoph Glechner</b>
+        <br>
+        Nr. 48
+        <br>
+        4926 St. Marienkirchen/H.
+        <br>
+        E-mail: <a href="mailto: c.glechner@gartenpro.at">c.glechner@gartenpro.at</a>
+        <br>
+        Mobil: 0650 4218586
+    </div>
+
+    <br>
+    <br>
+
+    <div>
+       <h4 style="margin-top: 0px;">
+            <a href="#muenzkirchen" name="muenzkirchen">Badmintonclub M&uuml;nzkirchen</a>
+        </h4>
+        <br>
+        Kennzahl: 331
+        <br>
+        Dachverband: ASV&Ouml;
+        <br>
+        <br>
+        <b>Josef Moser</b>
+        <br>
+        Am Haag 2
+        <br>
+        4792 M&uuml;nzkirchen
+        <br>
+        Internet: <a href="http://badmintonclub-muenzkirchen.at/">www.badminton-muenzkirchen.at</a>
+        <br>
+        E-mail: <a href="mailto: pepi-moser@gmx.at">pepi-moser@gmx.at</a>
+        <br>
+        Mobil: 0676 81424577
+    </div>
+    ';
 
     include("footer.php");
 ?>
