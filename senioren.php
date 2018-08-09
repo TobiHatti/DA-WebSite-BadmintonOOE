4615053acf358054206d89f7057f0cb129004b59
paull <paull@DESKTOP-DVL4DTN>
2018-08-09 03:14:58 +0200

    added seniors data until Senior Masters 2011

--- senioren.php
+++ senioren.php
@@ -2,7 +2,238 @@
     require("header.php");
     PageTitle("Senioren");
 
-    echo '<h1 class="stagfade1">Senioren</h1>';
+    echo '<h1 class="stagfade1">Senioren</h1>
+
+
+    <p>
+        <h3>Serior Master 2016</h3>
+        Der ober&ouml;sterreichische Badmintonverband und der Veranstalter Ask&Ouml; Traun richten das 4. O&Ouml;BV Senior Masters 2016 aus. Das Turnier ist ein reines Doppelturnier und wird am Samstag 14. Mai bis Sonntag 15. Mai 2016 ausgetragen. Ein besonderes Highlight wird die Players-Party mit Tombola und Tischzauberei sein. Nachfolgend die Details.
+    </p>
+
+    <p>
+       Veranstalter: Ober&ouml;sterreichischer Badminton Verband<br>
+       Ausrichter: ASK&ouml; Traun, www.badminton-traun.at<br>
+       Austragungsort: Bundesschulzentrum Traun, Schulstra&szlig;e 59/Eingang Kreuzfeldstra&szlig;e 7, 4050 Traun <br>
+       7 Spielfelder
+    </p>
+
+    <p>
+      Termin: Samstag, 14. Mai 2015 - Sonntag, 15. Mai 2016<br>
+      Beginnzeit: 10:00 Uhr (Samstag), 09:30 Uhr (Sonntag)<br>
+      Turnierablauf: Beginn mit MXD.<br>
+      Nach der Auslosung wird der Ablauf im Internet bekannt gegeben. Bewerbe: HD, DD und MXD<br>
+    </p>
+
+    <p>
+      Alterslimit: Die TeilnehmerInnen m&uuml;ssen vor dem 1.1.1981 geboren sein (&uuml;ber 35 Jahre)<br>
+      Altersklassen: z&auml;hlt das Gesamtalter der Paarung: 70+, 80+, 90+, 100+, 110+, 120+, 130+ (z.B. TeilnehmerIn-1: 35 Jahre und TeilnehmerIn-2: 40 Jahre = Altersklasse 70+)<br>
+      Wenn in einer Altersklasse weniger, als 4 Teilnehmer gemeldet sind, werden Altersklassen zusammengezogen.<br>
+      Spielmodus: Jede Paarung in der gegebenen Disziplin hat mind. 3 Spiele. Bei 4 bis 5 TeilnehmerInnen in der Altersklasse spielt jeder gegen jeden.<br>
+      B&auml;lle: m&uuml;ssen von den Teilnehmern gestellt werden. Als Richtlinie gilt die Ballzulassung des &ouml;BV. Andere B&auml;lle, z.B. Kunstoffb&auml;lle sind nur dann erlaubt, wenn der Gegner einverstanden ist.<br>
+      Nennungen: An Ask&Ouml; Traun Badminton: herbert.koch@gmx.at oder auf www.badminton-traun.at<br>
+      Spielerlizenz nicht unbedingt erforderlich!<br>
+      Nennschluss: Montag, 9. Mai 2016<br>
+      Nenngeld: &euro; 10,- pro Teilnehmer/in, je Bewerb<br>
+    </p>
+
+    <p>
+      Auskunft: Herbert Koch, herbert.koch@gmx.at , 0664-240 75 56<br>
+      Preise: Pokale f&uuml;r SiegerInnen, Medaillen und Urkunden f&uuml;r Platzierte, Sachpreise
+      &Uuml;bernachtung: Hotel Almtalerhof***, Leerwies 1, 4050 Traun, 07229-733 01-0, almtalerhof@aon.at, www.almtalerhof.at
+    </p>
+
+    <br>
+    <br>
+
+    <p>
+       <h3>Serior Master 2015</h3>
+       <b>Doppelturnier der besonderen Art</b><br>
+       Veranstalter: Ober&ouml;sterreichischer Badminton Verband
+    </p>
+
+    <p>
+      Ausrichter: <b>ASK&Ouml; Traun</b>
+    <p>
+
+    <p>
+      Austragungsort:<br>
+      <b>Bundesschulzentrum Traun</b> - 7 Spielfelder (Eingang Kreuzfeldstra&szlig;e 7)
+    </p>
+
+    <p>
+       Termin:<br>
+       <b>Samstag, 2. Mai 2015 - Sonntag, 3. Mai 2015</b>
+    </p>
+
+    <p>
+       Beginnzeit:<br>
+       10:00 Uhr (Samstag), 09:30 Uhr (Sonntag)
+    </p>
+
+    <p>
+       Turnierablauf:<br>
+       Beginn mit MXD. Nach der Auslosung wird der Ablauf im Internet bekannt gegeben.
+    </p>
+
+    <p>
+      Bewerbe: HD, DD und  MXD
+    </p>
+
+    <p>
+      Link zur Tournamentsoftware:<br>
+      <a href="http://obv.tournamentsoftware.com/sport/tournament.aspx?id=A2232258-138D-48C0-87E1-A4BCB8A3A3A1">Tournamentsoftware</a>
+    </p>
+
+    <p>
+      Alterslimit:<br>
+      Die Teilnehmer m&uuml;ssen vor dem 1.1.1980 geboren sein (&uuml;ber 35 Jahre)
+    </p>
+
+    <p>
+      Altersklassen:<br>
+      z&auml;hlt <b>das Gesamtalter der Paarung</b>: 70+, 80+, 90+, 100+, 110+, 120+, 130+<br>
+      (z.B. Teilnehmer-1: 35 Jahre und Teilnehmer-2: 40 Jahre = Altersklasse 70+)<br>
+      Wenn in einer Altersklasse weniger, als 4 Teilnehmer gemeldet sind, werden 2 Altersklassen zusammengezogen.
+    </p>
+
+    <p>
+      Spielmodus:<br>
+      Jede Paarung in der gegebenen Disziplin hat mind. 3 Spiele. Bei 4 bis 5 Teilnehmern in der Altersklasse spielt jeder gegen jeden. Sonst wird nach Schweizer System gelost
+    </p>
+
+    <p>
+      B&auml;lle:<br>
+      m&uuml;ssen von den Teilnehmern gestellt werden. Als Richtlinie gilt die Ballzulassung des &Ouml;BV.
+    </p>
+
+    <p>
+       Nennungen:<br>
+       direkt, vom Verein oder Verband an das Sekretariat des O&Ouml;BV: <a href="mailto: ooe-badmintonverband@aon.at">ooe-badmintonverband@aon.at</a>
+    </p>
+
+    <p>
+      Nennschluss: <b>Montag, 27. April 2015</b>
+    </p>
+
+    <p>
+     Nenngeld: &euro; 10,- pro Teilnehmer, je Bewerb
+    </p>
+
+    <p>
+      <b>Auslosung: Donnerstag, 30. April 2015</b>
+    </p>
+
+    <p>
+      Turnierleitung: ASK&Ouml; Traun
+    </p>
+
+    <p>
+      Oberschiedsrichter: Klaus Mitterhauser, Janos Halasz
+    </p>
+
+    <p>
+      Auskunft: Herbert Koch - <a href="mailto: herbert.koch@gmx.at">herbert.koch@gmx.at</a> - Handy 0664 2407556
+    </p>
+
+    <p>
+      &Uml;bernachtung: Hotel Almtalerhof***, Leerwies 1, 4050 Traun, Tel. +43/7229/73301-0, <a href="mailto: almtalerhof@aon.at">almtalerhof@aon.at</a> - <a href="https://www.almtalerhof.at/">www.almtalerhof.at</a>
+    </p>
+
+    <p>
+      Players Party: Samstag ca. 20:00 Uhr im Bundesschulzentrum mit Rahmenprogramm (Unkostenbeitrag &euro; 10,- f&uuml;r Speisen/Getr&auml;nke)
+    </p>
+
+    <br>
+    <br>
+
+    <p>
+        <h3>Serior Master 2012</h3>
+        Am 26./27.10.20112 fand bereits das zweite Senior Masters statt. Spielberechtigt waren alle Senioren ab 35 Jahre. 41 Teilnehmer aus 6 Bundesl&auml;ndern fanden sich am Nationalfeiertag in Linz/Urfahr ein.
+    </p>
+
+    <p>
+       <b>Sieger:<br>
+       Herrendoppel:</b><br>
+       HD 70 : Tarek El Dabbagh + Gerhard Mayr<br>
+       HD 100: Wolfgang Luber + Max Pichler<br>
+       HD 109: Joachim Bergner + Walter Struger<br>
+       HD Senior Super Finals: Tarek El Dabbagh + Gerhard Mayr
+    </p>
+
+    <p>
+       <b>Damendoppel:</b><br>
+       DD 80: Anja Friedrich + Elisabeth Pr&ouml;ll
+    </p>
+
+    <p>
+       <b>Mixdoppel:</b><br>
+       MD 70: Harald Friedrich + Anja Friedrich<br>
+       MD 100: Harald Starl + Waltraud Kronsteiner
+    </p>
+
+    <p>
+       <a href="http://obv.tournamentsoftware.com/sport/winners.aspx?id=CE262E3F-47CF-4FE5-AF0F-60698D930C67">Tournamentsoftware</a>
+    </p>
+
+    <br>
+    <br>
+
+    <p>
+       <h3>Serior Master 2011</h3>
+       Am 26.10.2011 fand, angeregt durch den O&Ouml;BV-Seniorenreferenten Janos Halasz, das erste Senior Masters in Neuhofen statt. Es sollte ein &Ouml;sterreich weites Turnier f&uuml;r den Altersbereich ab 35 Jahren durchgef&uuml;hrt werden.
+       47 Teilnehmer aus 7 Bundesl&auml;ndern fanden sich am Nationalfeiertag in Neuhofen in einem mit den ) 9 Landesfahnen sch&ouml;n geschm&uuml;ckten Turnsaal ein.
+       Die Er&ouml;ffnung machte Gerhard Brunnbaur (Union Vorstandsmitglied aus Neuhofen), der in seiner Ansprache &auml;u&szlig;erst lobend erw&auml;hnte, welch tolle Idee diese bundesl&auml;nder&uuml;bergreifende Veranstaltung sei. Anschlie&szlig;end begr&uuml;&szlig;te auch der Pr&auml;sident des O&Ouml;-Badmintonverbandes Herbert Koch die anwesenden Teilnehmer und Rabel Andreas er&ouml;ffnete dieses erste Senioren Masters.
+       Wie in der Ausschreibung schon erw&auml;hnt wurde das Alter der beiden Spieler zusammengez&auml;hlt und so kamen die einzelnen Klassen zustande.
+       Klaus Mitterhauser unterst&uuml;tzte uns am PC.
+    </p>
+
+    <p>
+       <b>Sieger:<br>
+       Herrendoppel:</b><br>
+       HD 70 : Markus Piribauer + Thanh Tran<br>
+       HD 90: Wolfgang Dornig + Peter Krenmayr<br>
+       HD 100: Joachim Berger + Walter Struger<br>
+       HD 110: Peter Golazewski + Johannes Lenhart
+    </p>
+
+    <p>
+       <b>Damendoppel:</b><br>
+       DD 80: Karin Steinbichler + Susanne Swoboda<br>
+       DD 100:Alexandra Pelz + Sylvia Weiss
+    </p>
+
+    <p>
+       <b>Mixdoppel:</b><br>
+       MD 70: Markus Piribauer + Karin Steinbichler<br>
+       MD 100: Karl Pr&ouml;ll + Elisabeth Pr&ouml;ll<br>
+       MX 110: Walter Struger + Hilde Kreulitsch
+    </p>
+
+    <p>
+       <b>MX 100 Super Finale: Pr&ouml;ll/Pr&ouml;ll - diese Paarung gewann den ersten fl&uuml;ssigen 0,33 Liter Pokal gesponsert von Eggenberger Bier</b>
+    </p>
+
+    <p>
+      Turnierende war gegen 16 Uhr mit Siegerehrung und gemeinsamen Foto.
+    </p>
+
+    <p>
+       Falls jemand Lust hat, sich Fotos vom Turnier mailen zu lassen, bitte kurzes Mail an: <a href="mailto: lind.p@aon.at">lind.p@aon.at</a>
+    </p>
+
+    <p>
+       Janos danken wir f&uuml;r seine gute Idee und den Teilnehmern f&uuml;r ihr faires Verhalten. Wir hoffen, dass dieses ins Leben gerufene Senioren Masters weiterhin bestehen bleibt und sich in Zukunft auch Veranstalter daf&uuml;r finden. Danke auch unseren zwei jungen Damen am Buffet, die ihre Aufgabe toll gemeistert haben.
+    </p>
+
+    <p>
+       <a href="http://obv.tournamentsoftware.com/sport/tournament.aspx?id=02C0AF40-8F72-4A6E-9D73-90B74AB0489C">Spiele auf der Tournamentsoftware</a><br>
+       Bericht: Peter Lindner/UBC Neuhofen
+    </p>
+
+
+
+
+    ';
 
     include("footer.php");
 ?>
