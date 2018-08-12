e067d6e6fd19daad982580264f616bad1a4d6862
paull <paull@DESKTOP-DVL4DTN>
2018-08-13 00:42:33 +0200

    tried to layout the nachwuchskalender (not successfull yet)

--- nachwuchskader.php
+++ nachwuchskader.php
@@ -1,11 +1,62 @@
 <?php
     require("header.php");
-    PageTitle("O&Ouml; Nachwuchskader");
+    PageTitle("OÖ Nachwuchskader");
 
     echo '<h1 class="stagfade1">O&Ouml; Nachwuchskader</h1>
 
+    <div class="entry">
+        <table>
+            <tbody>
+               <tr>
+                  <td width="217">
+                     <p style="text-align: center;">
+                         Burschen U19 - <br>
+                         2000-2001<br>
+                     </p>
+                  </td>
+               </tr>
+            </tbody>
+        </table>
 
+                  <td width="217">
+                    <p style="text-align: center;">
+                       M&auml;dchen U19 - <br>
+                       2000-2001<br>
+                    </p>
+                  </td>
 
+                  <td width="217">
+                    <p style="text-align: center;">
+                       Burschen U15 - <br>
+                       2004-2005<br>
+                    </p>
+                  </td>
+
+                  <td width="217">
+                    <p style="text-align: center;">
+                       M&auml;dchen U15 - <br>
+                       2004-2005<br>
+                    </p>
+                  </td>
+
+                  <td width="217">
+                    <p style="text-align: center;">
+                       Burschen U13 - <br>
+                       2006-2007<br>
+                    </p>
+                  </td>
+
+                  <td width="217">
+                    <p style="text-align: center;">
+                       M&auml;dchen U13 - <br>
+                       2006-2007<br>
+                    </p>
+                  </td>
+               </tr>
+            </tbody>
+        </table>
+
+    </div>
 
 
 
