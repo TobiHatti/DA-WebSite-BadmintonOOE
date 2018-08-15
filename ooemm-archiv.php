4cb889ad774b8d1b3707fc35e8037112b015abf6
paull <paull@DESKTOP-DVL4DTN>
2018-08-15 22:04:01 +0200

    started addind data to oömm archieve

--- ooemm-archiv.php
+++ ooemm-archiv.php
@@ -4,6 +4,140 @@
 
     echo '<h1 class="stagfade1">O&Ouml;MM-Archiv</h1>
 
+    <table>
+         <tbody>
+               <th>
+                  1. Landesliga
+               </th>
+           </tbody>
+               <tr>
+                   <td>
+                      <b>Pl</b>
+                   </td>
+                   <td>
+                      <b>Verein</b>
+                   </td>
+                   <td>
+                      <b>Rd</b>
+                   </td>
+                   <td>
+                      <b>S</b>
+                   </td>
+                   <td>
+                      <b>U</b>
+                   </td>
+                   <td>
+                      <b>N</b>
+                   </td>
+                   <td>
+                      <b>Spiele</b>
+                   </td>
+                   <td>
+                      <b>S&auml;tze</b>
+                   </td>
+                   <td>
+                      <b>Pkt</b>
+                   </td>
+               </tr>
+
+               <tr>
+                   <td>
+                      1
+                   </td>
+                   <td>
+                      ASK&Ouml; Traun
+                   </td>
+                   <td>
+                      10
+                   </td>
+                   <td>
+                      10
+                   </td>
+                   <td>
+                      0
+                   </td>
+                   <td>
+                      0
+                   </td>
+                   <td>
+                      71:9
+                   </td>
+                   <td>
+                      143:26
+                   </td>
+                   <td>
+                      30
+                   </td>
+               </tr>
+
+               <tr>
+                  <td>
+                     2
+                  </td>
+                  <td>
+                     UBC Sodian Vorchdorf II
+                  </td>
+                  <td>
+                     10
+                  </td>
+                  <td>
+                     6
+                  </td>
+                  <td>
+                     1
+                  </td>
+                  <td>
+                     3
+                  </td>
+                  <td>
+                     74:33
+                  </td>
+                  <td>
+                     99:74
+                  </td>
+                  <td>
+                     23
+                  </td>
+               </tr>
+
+               <tr>
+                  <td>
+                     3
+                  </td>
+                  <td>
+                     BC Raiffeisen Alkhoven I
+                  </td>
+                  <td>
+                     10
+                  </td>
+                  <td>
+                     5
+                  </td>
+                  <td>
+                     2
+                  </td>
+                  <td>
+                     3
+                  </td>
+                  <td>
+                     44:36
+                  </td>
+                  <td>
+                     97:73
+                  </td>
+                  <td>
+                     22
+                  </td>
+               </tr>
+
+               <tr>
+                  <td>
+                     4
+                  </td>
+               </tr>
+         </tbody>
+    </table>
+
 
 
     ';
