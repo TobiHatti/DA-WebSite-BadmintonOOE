886312215e0c5b6301d01cbce651d6c3c175b50e
paull <paull@DESKTOP-DVL4DTN>
2018-08-22 22:51:10 +0200

    started creating "Fotogalerie"

--- fotogalerie.php
+++ fotogalerie.php
@@ -2,7 +2,33 @@
     require("header.php");
     PageTitle("Fotogalierie");
 
-    echo '<h1 class="stagfade1">Fotogalierie</h1>';
+
+     if(isset($_POST['add_album']))
+     {
+        $albumName = $_POST['album_name'];
+        $today = date("Y-m-d");
+        $user = $_SESSION['user_id'];
+
+        MySQLNonQuery("INSERT INTO fotogalerie (ID,AlbumName, ErstellungsDatum, Autor) VALUES ('','$albumName','$today','$user')");
+
+        $albID = Fetch("fotogalerie","id","AlbumName",$albumName);
+        FileUpload("content/gallery/".SReplace($albumName)."/", "images" ,"","","INSERT INTO gallery_images (id,album_id,image) VALUES ('','$albID','FNAME')");
+    }
+
+
+
+    echo '<h1 class="stagfade1">Fotogalierie</h1>
+        <form action="'.ThisPage().'" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+        <br>
+        <input type="text" placeholder="Album Name" name="album_name"/>
+        <br>
+        <br>
+        <br>
+        '.FileButton("images", "element-id", 1).'
+        <br>
+        <button type="submit" name="add_album" value="post-value">Album hinzuf&uuml;gen
+       </form>
+    ';
 
     include("footer.php");
 ?>
