1589813f478896313fc5bac6959cafa0258ab711
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-08-24 00:09:27 +0200

    Enhanced News-Archiv

--- news-archiv.php
+++ news-archiv.php
@@ -2,36 +2,55 @@
     require("header.php");
     PageTitle("News-Archiv");
 
+    $year = (isset($_GET['jahr'])) ? $_GET['jahr'] : date("Y");
+
+
     echo '<h1 class="stagfade1">News-Archiv</h1>';
 
     echo '
         <center>
-        <div class="archive_year_selection">
-            <button onclick="ListMoveRight();"> < </button>
-            <div class="year_container">
-                <div class="year_slider" id="YearSlider">
+            <div class="archive_year_selection">
+                <a onclick="ListMoveRight();"><img src="/content/left.png" alt=""/></a>
+                <div class="year_container">
+                    <div class="year_slider" id="YearSlider">
 
-                    ';
+                        ';
+                        for($y = date("Y"); $y >= date("Y")-10;$y--)
+                        {
+                            echo '<a href="#">'.(($y == $year) ? ('<span>'.$y.'</span>') : $y ).'</a>';
+                        }
+                        echo '
+                    </div>
 
-                    for($year = date("Y"); $year >= date("Y")-10;$year--)
-                    {
-                        echo '<a href="#">'.$year.'</a>';
-                    }
-
-                    echo '
                 </div>
+                <a onclick="ListMoveLeft();"><img src="/content/right.png" alt=""/></a>
 
+                <input type="hidden" id="offsetIdx" value="0"/>
+                <input type="hidden" id="scrollWidth" value="'.(100*2).'"/>
             </div>
-            <button onclick="ListMoveLeft();"> > </button>
+            <hr style="margin-bottom:3px;">
+            <div class="archive_month_selection">
+                <a href="#">J&auml;nner</a>
+                <a href="#">Februar</a>
+                <a href="#">M&auml;rz</a>
+                <a href="#">April</a>
+                <a href="#">Mai</a>
+                <a href="#">Juni</a>
+                <a href="#">Juli</a>
+                <a href="#">August</a>
+                <a href="#">September</a>
+                <a href="#">Oktober</a>
+                <a href="#">November</a>
+                <a href="#">Dezember</a>
+            </div>
+            <hr style="margin-top:3px;">
 
-            <input type="hidden" id="offsetIdx" value="0"/>
-            <input type="hidden" id="scrollWidth" value="'.(100*2).'"/>
-        </div>
+
+            <br><br>
+
+            <iframe src="news-archiv-content" frameborder="1">
+            </iframe>
         </center>
-
-
-
-        Note: change following content in iframe
 
     ';
 
