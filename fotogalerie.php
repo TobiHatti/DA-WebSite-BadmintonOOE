ad03c9598693e15c86fb36b623a48a8062d7df9a
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-08-24 22:58:50 +0200

    Small bug-fixes Added Sample-Content for Fotogalerie

--- fotogalerie.php
+++ fotogalerie.php
@@ -9,7 +9,7 @@
         $today = date("Y-m-d");
         $user = $_SESSION['user_id'];
 
-        MySQLNonQuery("INSERT INTO  (ID,AlbumName, ErstellungsDatum, Autor) VALUES ('','$albumName','$today','$user')");
+        MySQLNonQuery("INSERT INTO fotogalerie (ID,AlbumName, ErstellungsDatum, Autor) VALUES ('','$albumName','$today','$user')");
 
         $albID = Fetch("fotogalerie","id","AlbumName",$albumName);
         FileUpload("content/gallery/".SReplace($albumName)."/", "images" ,"","","INSERT INTO gallery_images (id,album_id,image) VALUES ('','$albID','FNAME')");
@@ -34,80 +34,66 @@
     }
     else
     {
-         $strSQL = "SELECT * FROM fotogalerie";
-         $rs=mysqli_query($link,$strSQL);
-
-         while($row=mysqli_fetch_assoc($rs))
+        $strSQL = "SELECT * FROM fotogalerie";
+        $rs=mysqli_query($link,$strSQL);
+        while($row=mysqli_fetch_assoc($rs))
         {
-         echo'
-            <div class="gallery_album">
-                <div class="image_preview">
-                    ';
+            echo'
+                <div class="gallery_album">
+                    <div class="image_preview">
+            ';
 
-                    //Fotoanzahl 1
-                    $albumname = $row['AlbumName'];
-                    if(MySQLCount("SELECT id FROM fotogalerie WHERE AlbumName = '$albumname'")==1)
-                    {
-                     $id = $row['ID'];
-                     $strSQLI = "SELECT * FROM gallery_images INNER JOIN fotogalerie ON gallery_images.album_id = fotogalerie.ID WHERE fotogalerie.ID = '$id' LIMIT 0,1";
-                     $rsI=mysqli_query($link,$strSQLI);
-                     while($rowI=mysqli_fetch_assoc($rsI))
-                     {
-                         echo '<img src="/content/gallery/'.$row['AlbumName'].'/'.$rowI['image'].'" id="img1">';
-                     }
-                    }else if(MySQLCount("SELECT id FROM fotogalerie WHERE AlbumName = '$albumname'")>=4)
-                    {
-                        $i=1;
-                        $id = $row['ID'];
-                        $strSQLI = "SELECT * FROM gallery_images INNER JOIN fotogalerie ON gallery_images.album_id = fotogalerie.ID WHERE fotogalerie.ID = '$id' LIMIT 0,4";
-                        $rsI=mysqli_query($link,$strSQLI);
-                        while($rowI=mysqli_fetch_assoc($rsI))
-                        {
-                            echo '<img src="/content/gallery/'.$row['AlbumName'].'/'.$rowI['image'].'" id="img'.$i.'">';
-                            $i++;
-                        }
-                    }
+            //Fotoanzahl 1
+            $albumId = $row['ID'];
 
-                    else if(MySQLCount("SELECT id FROM fotogalerie WHERE AlbumName = '$albumname'")>=9)
-                    {
-                        $i=1;
-                        $id = $row['ID'];
-                        $strSQLI = "SELECT * FROM gallery_images INNER JOIN fotogalerie ON gallery_images.album_id = fotogalerie.ID WHERE fotogalerie.ID = '$id' LIMIT 0,9";
-                        $rsI=mysqli_query($link,$strSQLI);
-                        while($rowI=mysqli_fetch_assoc($rsI))
-                        {
-                            echo '<img src="/content/gallery/'.$row['AlbumName'].'/'.$rowI['image'].'" id="img'.$i.'">';
-                            $i++;
-                        }
+            if(MySQLCount("SELECT id FROM gallery_images WHERE album_id = '$albumId'")>=9)
+            {
+                $i=1;
+                $id = $row['ID'];
+                $strSQLI = "SELECT * FROM gallery_images INNER JOIN fotogalerie ON gallery_images.album_id = fotogalerie.ID WHERE fotogalerie.ID = '$id' LIMIT 0,9";
+                $rsI=mysqli_query($link,$strSQLI);
+                while($rowI=mysqli_fetch_assoc($rsI))
+                {
+                    echo '<img src="/content/gallery/'.$row['AlbumName'].'/'.$rowI['image'].'" id="img'.$i.'">';
+                    $i++;
+                }
+            }
+            else if(MySQLCount("SELECT id FROM gallery_images WHERE album_id = '$albumId'")>=4)
+            {
+                $i=1;
+                $id = $row['ID'];
+                $strSQLI = "SELECT * FROM gallery_images INNER JOIN fotogalerie ON gallery_images.album_id = fotogalerie.ID WHERE fotogalerie.ID = '$id' LIMIT 0,4";
+                $rsI=mysqli_query($link,$strSQLI);
+                while($rowI=mysqli_fetch_assoc($rsI))
+                {
+                    echo '<img src="/content/gallery/'.$row['AlbumName'].'/'.$rowI['image'].'" id="img'.$i.'">';
+                    $i++;
+                }
+            }
+            else if(MySQLCount("SELECT id FROM gallery_images WHERE album_id = '$albumId'") < 4)
+            {
+                $id = $row['ID'];
+                $strSQLI = "SELECT * FROM gallery_images INNER JOIN fotogalerie ON gallery_images.album_id = fotogalerie.ID WHERE fotogalerie.ID = '$id' LIMIT 0,1";
+                $rsI=mysqli_query($link,$strSQLI);
+                while($rowI=mysqli_fetch_assoc($rsI))
+                {
+                    echo '<img src="/content/gallery/'.$row['AlbumName'].'/'.$rowI['image'].'" id="img1">';
+                }
+            }
 
-                    }
-                      	                // Mit MySQLCount() abfragen, wieviele Fotos in einem Album vorhanden sind.
-                                        // Je nach Anzahl bestimmte bedingung auslösen:
-                                        // Fotoanzahl == 1  (1 Foto wird in der Vorschau angezeigt)
-                                        // Fotoanzahl >= 4  (4 Fotos werden in der Vorschau angezeigt)
-                                        // Fotoanzahl >= 9  (9 Fotos werden in der Vorschau angezeigt)
 
-                                        // Tipp:
-                                        //
-                                        // if(MySQLCount("SELECT id FROM fotogalerie WHERE AlbumName = '$albumname'") >=9)
-                                        // { ... }
-                                        // else if(...)
-                                        // { ... }
-                                        // ...
-
-                                        echo '
-                                    </div>
-                                    <div class="album_description">
-                                        <h3> '.$row['AlbumName'].'</h3>
-                                        <p>
-                                        	Beschreibung hier
-                                        </p>
-                                    </div>
-                                </div>
-
-                                ';
-                            }
-                        }
+            echo '
+                    </div>
+                    <div class="album_description">
+                        <h3> '.$row['AlbumName'].'</h3>
+                        <p>
+                            Beschreibung hier
+                        </p>
+                    </div>
+                </div>
+            ';
+        }
+    }
 
 
     include("footer.php");
