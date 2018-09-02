31daba1974c3cd0021122a398911453931065b7a
Paul Luger <paull@DESKTOP-DVL4DTN>
2018-09-02 22:32:37 +0200

    adjusting a new agenda system (currently adding new individual agendas)

--- agenda.php
+++ agenda.php
@@ -4,11 +4,7 @@
 
 
 
-    echo '
-        <center>
-            <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=de.christian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=de.austrian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=e_2_de%23weeknum%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Europe%2FVienna" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-        </center>
-    ';
+
 
 
    echo '<main>';
@@ -23,7 +19,7 @@
 </head>
 
     <center>
-    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=de.christian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=de.austrian%23holiday%40group.v.calendar.google.com&amp;color=%230F4B38&amp;src=e_2_de%23weeknum%40group.v.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Europe%2FVienna" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://calendar.google.com/calendar/embed?src=cbjpthnc43ltk0dbevutalhlqo%40group.calendar.google.com&ctz=Europe%2FVienna" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </center>
 <?php
 
