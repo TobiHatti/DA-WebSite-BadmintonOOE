98e703500482a2352ccb97699a2f43d173bb50cf
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-09-17 22:42:26 +0200

    Started user-register form in settings Bug-fix in fotogalerie

--- settings.php
+++ settings.php
@@ -7,12 +7,12 @@
 
         <div class="settings_content">
             <div class="menu">
-                <span>Menu</span>
+                <span>Men&uuml;</span>
                 <a onclick="ChangeFrameLink(\'settings_content?topic=Unsorted\');"><li>Unsortiert</li></a>
                 <a onclick="ChangeFrameLink(\'settings_content?topic=Allgemein\');"><li>Allgemein</li></a>
                 <a onclick="ChangeFrameLink(\'settings_content?topic=Startseite\');"><li>Startseite</li></a>
                 <a onclick="ChangeFrameLink(\'settings_content?topic=Nutzer\');"><li>Nutzer</li></a>
-                <a onclick="ChangeFrameLink(\'settings_content?topic=Permissions\');"><li>Permissions</li></a>
+                <a onclick="ChangeFrameLink(\'settings_content?topic=Rechte\');"><li>Rechte</li></a>
             </div>
             <iframe id="chframe" name="settingsContent" src="/settings_content?topic=Unsorted" frameborder="0"></iframe>
         </div>
