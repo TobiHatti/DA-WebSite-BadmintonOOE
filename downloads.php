b979e3b724596c0cde3ac2c6895651753be9d1e9
Paul Luger <paul.luger@gmx.at>
2018-10-20 21:21:32 +0200

    started optimizing download.php, in economic aspects

--- downloads.php
+++ downloads.php
@@ -2,11 +2,18 @@
     require("header.php");
     PageTitle("Downloads");
 
+    if(isset($_POST['add_data'])){
+
+    }
     echo '
         <h1 class="stagfade1">Downloads</h1>
         <p>
            '.PageContent('1',CheckPermission("ChangeContent")).'
         </p>
+        <div>
+            <br>
+            <button type="submit" name="add_data" value="post-value">Datei ausw&auml;hlen
+        </div>
     ';
 
     include("footer.php");
