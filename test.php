4319bb4c4b8253da80c7918dfb4828ca4b831180
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-11-10 09:38:26 +0100

    Improved MySQLNonQuery-Fuction
    -Added Parameterized option

--- test.php
+++ test.php
@@ -2,11 +2,34 @@
     include("header.php");
 
 
-    echo ''.ColorPicker("","picker1","Farbe w&auml;hlen", "#FFD700") .'';
+    MySQLNonQuery("INSERT INTO test (id,value) VALUES ('','Hi')");
+
+    $value = "Ho";
+    $value2 = "He";
+
+    //MySQLNonQuery("INSERT INTO test (id,value,value2) VALUES ('',?,?)","@s",$value,$value2);
 
 
 
     include("footer.php");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
 
