95c424c827e4f4ef97ffbeb0ebbf823d740972de
Tobias Hattinger <tobias.hattinger@gmx.at>
2019-02-06 17:57:07 +0100

    Added IE-Support notice

---
+++ ie-support.php
@@ -0,0 +1,83 @@
+
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <style type="text/css">
+        html{
+            font-family: Calibri, Arial, sans-serif;
+        }
+
+            body{
+                background-image: url(content/header2.jpg);
+                background-clip: border-box;
+                background-repeat:  repeat-y;
+                background-size: cover;
+
+                color: #FFFFFF;
+            }
+
+            body div{
+                position: fixed;
+                top: 0;
+                left:0 ;
+                right: 0;
+                bottom: 0;
+
+                background: rgba(10, 0, 0, 0.8)
+            }
+
+            h1{
+                font-size: 100pt;
+                font-weight: lighter;
+            }
+
+            h3{
+                font-weight: lighter;
+            }
+
+        </style>
+
+        <title>Bitte wechseln Sie den Browser</title>
+    </head>
+    <body>
+        <div>
+
+            <center>
+                <h1>Bitte beachten</h1>
+
+                <h2>Die <i>Internet-Explorer</i> Version dieser Seite ist aktuell noch nicht verf&uuml;gbar.<br>
+                <br>
+                Versuchen Sie es erneut mit einem anderen Browser, wie z.B.:
+                    <ul>
+                        <li>Microsoft Edge</li>
+                        <li>FireFox</li>
+                        <li>Google Chrome</li>
+                        <li>Opera</li>
+                        <li>Safari</li>
+                    </ul>
+                <br>
+                Wir entschuldigen uns f&uuml;r Unannehmlichkeiten.<br>
+                <br>
+                <span style="color: #1E90FF">Das Entwicklerteam<br>
+                Tobias Hattinger<br>
+                Paul Luger</span>
+
+                <br><br>
+                </h2>
+                <h3>
+                    <u>Warum wird Internet-Explorer noch nicht unterst&uuml;tzt? </u>
+                    <br>
+                    <b><i>Internet Explorer</i></b> ist im vergleich zu anderen Browsern wie <b><i>Chrome</i></b> oder <b><i>Firefox</i></b> ein sehr veralteter und schwer unterst&uuml;tzbarer Browser.<br>
+                    Um Internet Explorer unterst&uuml;tzen zu k&ouml;nnen, muss ein großteil des Seitendesigns erneut erstellt werden, ein oft sehr zeitaufw&auml;ndiger und arbeitsintensiver Vorgang<br>
+                    <br>
+                    Da nur noch  rund 6% der Internet-Nutzer diesen Browser verwenden, wird immer &ouml;fter auf eine Internet-Explorer unterst&uuml;tzung verzichtet.<br>
+                    Wir werden versuchen die unterstützung für Internet-Explorer so schnell wie möglich zu realisieren, werden jedoch den Fokus auf andere Browser legen.
+                </h3>
+            </center>
+
+        </div>
+    </body>
+</html>
