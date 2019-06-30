72d8d1e58aa458dd84a144d42fb0295b84520a61
Tobias Hattinger <tobias.hattinger@gmx.at>
2019-07-01 00:07:04 +0200

    Removed unnecessary backup-files

--- backup/dbbu_D2018-07-27.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `club` (  `id` int(11) NOT NULL AUTO_INCREMENT,  `name` int(11) NOT NULL,  PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-INSERT INTO `club` VALUES('2', '123');
-INSERT INTO `club` VALUES('3', '123');
-INSERT INTO `club` VALUES('4', '123');
-CREATE TABLE `settings` (  `setting` varchar(255) COLLATE utf8_unicode_ci NOT NULL,  `value` varchar(255) COLLATE utf8_unicode_ci NOT NULL,  PRIMARY KEY (`setting`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-CREATE TABLE `vorstand` (  `id` int(11) NOT NULL AUTO_INCREMENT,  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,  `bereiche` varchar(255) COLLATE utf8_unicode_ci NOT NULL,  `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,  `telefon` varchar(20) COLLATE utf8_unicode_ci NOT NULL,  PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-INSERT INTO `vorstand` VALUES('1', 'aaa', 'aaa', 'aaa', 'aaa');
-INSERT INTO `vorstand` VALUES('2', 'bbb', 'bbb', 'bbb', 'bbb');
